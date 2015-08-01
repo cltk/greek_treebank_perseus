ac72830fe489025a9d8ed1e97509fbc676af2c53
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-07-31 23:06:23 -0700

    re-train pos set and models with non-repeated training set

---
+++ make_pos_models.py
@@ -0,0 +1,53 @@
+"""Train cltk POS models from a training set and save them in the model repository."""
+
+from nltk.corpus.reader import TaggedCorpusReader
+from nltk.tag import UnigramTagger
+from nltk.tag import BigramTagger
+from nltk.tag import TrigramTagger
+from nltk.tag import tnt
+import os
+import pickle
+import time
+
+
+def make_pos_model(model_type):
+    now = time.time()
+
+    reader = TaggedCorpusReader('.', 'greek_training_set.pos')
+    train_sents = reader.tagged_sents()
+    if model_type == 'unigram':
+        tagger = UnigramTagger(train_sents)
+        file = 'unigram.pickle'
+    elif model_type == 'bigram':
+        tagger = BigramTagger(train_sents)
+        file = 'bigram.pickle'
+    elif model_type == 'trigram':
+        tagger = TrigramTagger(train_sents)
+        file = 'trigram.pickle'
+    elif model_type == 'backoff':
+        tagger1 = UnigramTagger(train_sents)
+        tagger2 = BigramTagger(train_sents, backoff=tagger1)
+        tagger = TrigramTagger(train_sents, backoff=tagger2)
+        file = '123grambackoff.pickle'
+    elif model_type == 'tnt':
+        tagger = tnt.TnT()
+        tagger.train(train_sents)
+        file = 'tnt.pickle'
+    else:
+        print('Invalid model_type.')
+
+    _dir = os.path.expanduser('~/greek_models_cltk/taggers/pos')
+    path = os.path.join(_dir, file)
+    with open(path, 'wb') as f:
+        pickle.dump(tagger, f)
+
+    print('Completed training {0} model in {1} seconds to {2}.'.format(model_type, time.time() - now, path))
+
+    #print('Accuracy:', tagger.evaluate(train_sents))
+
+if __name__ == "__main__":
+    make_pos_model('unigram')
+    make_pos_model('bigram')
+    make_pos_model('trigram')
+    make_pos_model('backoff')
+    make_pos_model('tnt')
--- make_pos_training_set.py
+++ make_pos_training_set.py
@@ -1,58 +1,42 @@
-from cltk.corpus.classical_greek.beta_to_unicode import Replacer
 from lxml import etree
 import os
-
 
 __author__ = ['Kyle P. Johnson <kyle@kyle-p-johnson.com>', 'Stephen Margheim <stephen.margheim@gmail.com>']
 __license__ = 'MIT License. See LICENSE.'
 
 
-r = Replacer()
-
-
-def get_files():
-    """Return a Generator of the Perseus Greek Treebank XML files
-    
-    """
-    files = os.listdir('treebank_perseus_greek')
-    for file in files:
-        if file.endswith('.xml'):
-            yield file
-
-
 def get_tags():
-    treebank_training_set = []
-    for xml_file in get_files():
-        file_path = os.path.join('treebank_perseus_greek/', xml_file)
-        with open(file_path, 'r') as f:
-            xml_string = f.read()
-        root = etree.fromstring(xml_string)
-        sentences = root.findall('sentence')
+    entire_treebank = 'greek_treebank_perseus/agdt-1.7.xml'
+    with open(entire_treebank, 'r') as f:
+        xml_string = f.read()
+    root = etree.fromstring(xml_string)
+    sentences = root.findall('sentence')
 
-        sentences_list = []
-        for sentence in sentences:  # note: sentence is Element
-            words_list = sentence.findall('word')
-            sentence_list = []
-            for x in words_list:  #note: word is class
-                word = x.attrib
-                form = word['form'].upper()  # make upper case for Beta Code converter
-                uni_form = r.beta_code(form)
-                try:  # convert final sigmas
-                    if uni_form[-1] == 'σ':
-                        uni_form = uni_form[:-1] + 'ς'
-                except IndexError:
-                    pass
-                postag = word['postag']  # note: postag is str
-                word_tag = '/'.join([uni_form, postag])
-                sentence_list.append(word_tag)
-            tagged_sentence = ' '.join(sentence_list)
-            sentences_list.append(tagged_sentence)
-        tagged_sentences = '\n\n'.join(sentences_list)
-        treebank_training_set.append(tagged_sentences)
-    pos_training_set = '\n\n'.join(treebank_training_set)
+    sentences_list = []
+    for sentence in sentences:  # note: sentence is Element
+        words_list = sentence.findall('word')
+        sentence_list = []
+        # http://ilk.uvt.nl/conll/
+        for x in words_list:  # note: word is class
+            word = x.attrib
+            #id = word['id']
+            form = word['form'].lower()
+            #lemma = word['lemma']
+            #cpostag = word['relation']  # Coarse-grained part-of-speech tag
+            postag = word['postag']
+            #feats = '_'  # an underscore if not available
+            #head = word['head']
+            #deprel = word['head']
+            #phead = '_'
+            #pderprel = '_'
+            word_tag = '/'.join([form, postag])
+            sentence_list.append(word_tag)
+        sentence_str = ' '.join(sentence_list)
+        sentences_list.append(sentence_str)
+    treebank_training_set = '\n\n'.join(sentences_list)
 
-    with open('pos_training_set.pos', 'w') as f:
-        f.write(pos_training_set)
+    with open('greek_training_set.pos', 'w') as f:
+        f.write(treebank_training_set)
 
 
 def main():
