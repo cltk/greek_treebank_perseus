e0503cd09202c9e01df0a23c1756fb82985a15ea
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-07-31 23:51:38 -0700

    improved training set

--- make_pos_training_set.py
+++ make_pos_training_set.py
@@ -1,11 +1,12 @@
+from cltk.corpus.greek.beta_to_unicode import Replacer
 from lxml import etree
-import os
 
 __author__ = ['Kyle P. Johnson <kyle@kyle-p-johnson.com>', 'Stephen Margheim <stephen.margheim@gmail.com>']
 __license__ = 'MIT License. See LICENSE.'
 
 
 def get_tags():
+    r = Replacer()
     entire_treebank = 'greek_treebank_perseus/agdt-1.7.xml'
     with open(entire_treebank, 'r') as f:
         xml_string = f.read()
@@ -20,7 +21,19 @@ def get_tags():
         for x in words_list:  # note: word is class
             word = x.attrib
             #id = word['id']
-            form = word['form'].lower()
+            form = word['form'].upper()  # make upper case for Beta Code converter
+            form = r.beta_code(form)
+            try:  # convert final sigmas
+                if form[-1] == 'σ':
+                    form = form[:-1] + 'ς'
+            except IndexError:
+                pass
+            form = form.lower()
+
+            # rm nasty single quotes
+            form_list = [char for char in form if char not in ["'", '᾽', '’', '[', ']']]
+            form = ''.join(form_list)
+
             #lemma = word['lemma']
             #cpostag = word['relation']  # Coarse-grained part-of-speech tag
             postag = word['postag']
